1121fa5000ca2f381cee9066f7179914d7697332
KimDongWook <dream4gstar@gmail.com>
2020-05-05 01:15:48 +0900

    wallet_login
    klaytn contract connected, wallet login UI

--- READ/webpack.config.js
+++ READ/webpack.config.js
@@ -1,21 +1,9 @@
-const webpack = require('webpack')
-const path = require('path')
-const fs = require('fs')
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: 'development',
+  entry: './index.js',
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, 'dist')   
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
-  plugins: [   
-    new webpack.DefinePlugin({
-       DEPLOYED_ADDRESS: JSON.stringify(fs.readFileSync('deployedAddress', 'utf8').replace(/\n|\r/g, "")),
-       DEPLOYED_ABI: fs.existsSync('deployedABI') && fs.readFileSync('deployedABI', 'utf8'),
-     }),
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html"}])
-  ],
-  devServer: { contentBase: path.join(__dirname, "dist"), compress: true }
-}
+};
